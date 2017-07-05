ecc42c7b8dace5535149b5781fc946b2bcaf192f
Christian Becher <phaziz@gmail.com>
2017-07-05 10:41:58 +0200

    ReadSave RichTextBox
    Read the content from a RichTextBox and save to a file. Open a file, read the content and draw it into a RichTextBox.

---
+++ Text.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace _20170705Aufgabe
+{
+    class Text
+    {
+        private string _Inhalt;
+        public string Inhalt
+        {
+            get { return _Inhalt; }
+            set { _Inhalt = value; }
+        }
+
+        private string _Dateiname;
+        public string Dateiname
+        {
+            get { return _Dateiname; }
+            set { _Dateiname = value; }
+        }
+
+        private string _PFadangabe;
+        public string Pfadangabe
+        {
+            get { return _PFadangabe; }
+            set { _PFadangabe = value; }
+        }
+        
+        public Text(string Inhalt, string Dateiname)
+        {
+            this.Inhalt = Inhalt;
+            this.Dateiname = Dateiname;
+        }
+
+        public string TextLesen()
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Alle Dateien(*.*)|*.*|Textdateien(*.txt)|*.txt";
+            ofd.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                FileInfo FileInfo = new FileInfo(ofd.FileName);
+                Pfadangabe = FileInfo.DirectoryName.ToString();
+                Dateiname = FileInfo.Name;
+                Inhalt = File.ReadAllText(Pfadangabe + '\\' + Dateiname);
+            }
+            else
+            {
+                Inhalt = "Inhalt konnte nicht gelesen werden, oder Aktion wurde abgebrochen...";
+            }
+
+            return Inhalt;
+        }
+
+        public int TextSpeichern(string T)
+        {
+            Inhalt = T;
+
+            using (var sfd = new SaveFileDialog())
+            {
+                string today = DateTime.Now.ToString("yyyy-MM-dd-hh-mm-ss");
+
+                sfd.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                sfd.FilterIndex = 2;
+                sfd.FileName = today;
+                sfd.DefaultExt = "txt";
+                sfd.AddExtension = true;
+                sfd.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    File.WriteAllText(sfd.FileName, Inhalt);
+
+                    return 0;
+                }
+                else
+                {
+                    return 1;
+                }
+            }
+        }
+    }
+}
